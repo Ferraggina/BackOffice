2cdda598a9c104228c28fe0903adac5b5e10ff2d
Dario Andres <dario26081993@outlook.com.ar>
2023-09-07 18:01:52 -0300

    sass y actions listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getDogs } from "./redux/actions/actions";
+
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getDogs())
+  },[])
+
   return (
     <>
       <h1>Hola soy prueba</h1>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./Reducer";
+import rootReducer from "../reducers/reducers";
 import thunkMiddleware from "redux-thunk";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
